e528cf88a4ab6dd19d3fe62e23dae8301f5d6b86
AndreyErmolaev <andrerm67@yandex.ru>
2023-09-26 20:54:28 +0300

    Исправлено по рьвью №1

--- Praktikum.java
+++ Praktikum.java
@@ -15,14 +15,13 @@ public class Praktikum {
     }
 
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        }else if (year % 100 == 0){
             return false;
         }else if (year % 4 == 0){
             return true;
-        }else if (year % 400 == 0){
-            return true;
-        }
-        else{
+        }else{
             return false;
         }// здесь нужно определить, является ли переданный год високосным
     }
